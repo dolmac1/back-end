07e97271a99084426e909c321b95be4d173657f2
dolmac <dolmac@naver.com>
2021-06-16 21:12:20 +0900

    1일차

---
+++ 인강/java programming/BinaryTest.java
@@ -0,0 +1,11 @@
+package ch04;
+
+public class BinaryTest {
+	public static void main(String[] args) {
+		int num = 10;
+		int bnum = 0B1010;
+		int hnum = 012;
+		int xnum = 0XA;
+		System.out.println(num+" "+bnum+" "+hnum+" "+xnum);
+	}
+}
---
+++ 인강/java programming/VariableTest.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		byte bnum = 127;
+		System.out.println(bnum);
+		//int num = 1234567900; //너무 길어서 안된다
+		long num = 1234567900l;
+		System.out.println(num);
+	}
+
+}
