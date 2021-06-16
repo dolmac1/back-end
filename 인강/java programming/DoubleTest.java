296b237ea11869e4fd397b1b14d0b3bf51eb2469
dolmac <dolmac@naver.com>
2021-06-16 21:39:20 +0900

    실수형 추가

---
+++ 인강/java programming/DoubleTest.java
@@ -0,0 +1,10 @@
+package ch07;
+
+public class DoubleTest {
+	public static void main(String[] args) {
+		double dnum = 3.14;
+		float fnum = 3.14f;
+		
+		System.out.println(dnum + " " + fnum);
+	}
+}
