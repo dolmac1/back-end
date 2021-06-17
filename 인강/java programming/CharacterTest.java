4729321a5127763e8f4ac0dec75a681ce03b74eb
dolmac <dolmac@naver.com>
2021-06-17 22:11:35 +0900

    자료형 끝

---
+++ 인강/java programming/CharacterTest.java
@@ -0,0 +1,10 @@
+package ch08;
+
+public class CharacterTest {
+	public static void main(String[] args) {
+		char A = 'A';
+		String As = "A";
+		
+		System.out.println(A + " " + As);
+	}
+}
---
+++ 인강/java programming/LocalVariable.java
@@ -0,0 +1,13 @@
+package ch09;
+
+public class LocalVariable {
+	public static void main(String[] args) {
+		var i = 10;
+		
+		System.out.println(i);
+		
+		i = 20;
+		System.out.println(i);
+		//i = "asaaa"
+	}
+}
