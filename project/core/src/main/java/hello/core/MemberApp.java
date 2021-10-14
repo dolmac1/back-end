cd671909b3860839950c5ee3ff42276f33abb395
dolmac <dolmac@naver.com>
2021-10-14 21:35:31 +0900

    3강끝

--- project/core/src/main/java/hello/core/MemberApp.java
+++ project/core/src/main/java/hello/core/MemberApp.java
@@ -1,18 +1,22 @@
 package hello.core;
-
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        Member member = new Member(1L, "dolmac", Grade.VIP);
-        memberService.join(member);
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //appconfig에 있는 환경 정보를 가지고 관리해주겠다.
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
 
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
         Member findMember = memberService.findMember(1L);
-        System.out.println(member.getName());
-        System.out.println(findMember.getName());
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
     }
 }
--- project/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ project/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,17 +1,15 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    @Test
-    void join(){
-        Member member = new Member(1L,"dolmac",Grade.VIP);
-
-        memberService.join(member);
-        Member findMember = memberService.findMember(1L);
-        Assertions.assertThat(member).isEqualTo(findMember);
-        System.out.println(member.getName());
+class MemberServiceTest {
+    MemberService memberService;
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
     }
 }
